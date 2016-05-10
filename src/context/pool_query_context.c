a7ad8156b6114f708090123bf6eaaecb2d49c2da
Roy Zheng <roycraft3@gmail.com>
2016-05-10 00:05:32 +0000

    Add SET CHARACTERISTIC and multistatement SET passthrough to psql
    This change allows JDBCv2 clients to work by allowing SET TRANSACTION
    statements such as SET SESSION CHARACTERISTIC as well as
    multistatement queries containing SET to route to psql rather than
    Presto.

--- src/context/pool_query_context.c
+++ src/context/pool_query_context.c
@@ -435,7 +435,13 @@ void pool_where_to_send(POOL_QUERY_CONTEXT *query_context, char *query, Node *no
 			{
 				ereport(DEBUG1, (errmsg("prestogres: send_to_where: multi-statement: statement %d", i)));
 				Node* stmt = (Node*) lfirst(cell);
-				PRESTOGRES_DEST dest = prestogres_send_to_where(stmt);
+				PRESTOGRES_DEST dest;
+				if (IsA(stmt, VariableSetStmt)) {
+				  dest = PRESTOGRES_SYSTEM;
+				}
+				else {
+				  prestogres_send_to_where(stmt);
+				}
 				switch (dest) {
 				case PRESTOGRES_SYSTEM:
 					merged_dest = PRESTOGRES_SYSTEM;
@@ -1809,7 +1815,7 @@ PRESTOGRES_DEST prestogres_send_to_where(Node *node)
 	 * CREATE TABLE ... AS SELECT
 	 * SET
 	 */
-        if (IsA(node, SelectStmt) || IsA(node, InsertStmt) || IsA(node, CreateStmt) || IsA(node, CreateTableAsStmt) || IsA(node, DropStmt) || IsA(node, ViewStmt) || IsA(node, VariableSetStmt))
+  if (IsA(node, SelectStmt) || IsA(node, InsertStmt) || IsA(node, CreateStmt) || IsA(node, CreateTableAsStmt) || IsA(node, DropStmt) || IsA(node, ViewStmt) || IsA(node, VariableSetStmt))
 	{
 		if (pool_has_system_catalog(node))
 		{
@@ -1841,6 +1847,16 @@ PRESTOGRES_DEST prestogres_send_to_where(Node *node)
 		{
 			ereport(DEBUG1, (errmsg("prestogres_send_to_where: no relations")));
 			return PRESTOGRES_EITHER;
+		}
+		/*
+		 * SET SESSION CHARACTERISTICS AS TRANSACTION
+		 * SET TRANSACTION
+		 */
+		if (!strcasecmp(((VariableSetStmt *)node)->name, "SESSION CHARACTERISTICS") ||
+		    !strcasecmp(((VariableSetStmt *)node)->name, "TRANSACTION")) {
+
+			ereport(DEBUG1, (errmsg("prestogres_send_to_where: set session characteristics as transaction")));
+			return PRESTOGRES_SYSTEM;
 		}
 
 		ereport(DEBUG1, (errmsg("prestogres_send_to_where: select, insert, create table")));
